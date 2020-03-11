608a8f4ee60642b20c3ea5dcef897ab3cdde98a8
Jason Graziano <mrehawk82@gmail.com>
2020-03-11 16:57:15 -0400

    file uploader with display

---
+++ app.js
@@ -0,0 +1,35 @@
+var http = require('http'),
+    express = require('express'),
+    fs = require('fs'),
+    formidable = require('formidable');
+
+var app = express();
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '\\uploads\\' + file.name;
+    });
+
+    form.on('file', function (name, file){
+        var fn = '<img src="/'+file.name+'" height="50" width="auto" />';
+        fs.appendFile('log.html', fn, function (err) {
+          if (err) throw err;
+        });
+        console.log('Uploaded ' + fn);
+    });
+
+    res.sendFile(__dirname + '/uploaded.html');
+});
+app.get('/assets', function (req, res){
+    app.use(express.static(__dirname + '/uploads'));
+    res.sendFile(__dirname + '/log.html');
+});
+app.listen(8080);
